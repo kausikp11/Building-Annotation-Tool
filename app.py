c5b07ebdbfbe03d5916f0bc5074d53f7b1812bdd
p.kausik <p.kausik@tcs.com>
2026-02-09 16:21:25 +0530

    added the string conversion

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ async def annotation_input(data:Annotation):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
-    blob.upload_from_string(data,)
+    blob.upload_from_string(data.model_dump_json(),)
     return data
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
